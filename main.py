609c1844a5a41071c462a8b3a26d36577372899a
QUANG <dodiu327@gmail.com>
2025-01-09 16:41:05 +0700

    Update main.py
    v1.1

--- main.py
+++ main.py
@@ -4,10 +4,12 @@
 
 
 from tkinter import *
+import tkinter.filedialog as fd
 import tkinter as tk
 from tkinter import messagebox as mb
 import os
-from datetime import date
+import random
+from datetime import datetime
 path='Database(QUANG).txt'
 
 def add():
@@ -16,8 +18,13 @@ def add():
     show()
 
 def show():
-    name=read() 
+    name=read()
+    messages = ['----- Have a nice day ! -----', '----- Hello and welcome! -----', '----- Hey there! You are the new face around here, right? -----', '----- Hey there, welcome! We are about to make your day a little brighter. -----', '----- Hey there! Just wanted to say a big welcome. -----']
+    movie = random.choice(messages)
+    copyrights ='----- COPYRIGHT © 2025 QUANG. ALL RIGHTS RESERVED. -----'
     listbox.delete(0,END)
+    listbox.insert('end',copyrights)
+    listbox.insert('end',movie)
     for i in name:
         listbox.insert(END,i)
 
@@ -27,6 +34,16 @@ def save(line):
         f = open(path,'a',encoding='utf8')
         f.writelines(line)
         f.writelines('\n')
+        f.close()
+    except:
+        with open("Database(QUANG).txt", "w") as file:
+            file.close()
+
+
+def openfile(line):
+    try:
+        f = open(path,'a',encoding='utf8')
+        f.writelines(line)
         f.close()
     except:
         with open("Database(QUANG).txt", "w") as file:
@@ -106,19 +123,39 @@ def msg():
         if res == 'yes' :
             root.destroy()
 
-today = date.today()
+def imports():
+    files = fd.askopenfilenames(parent=root,title='Choose files',filetypes=[('Database', '.txt')])
+    for file in files:
+        with open(file, 'r') as f:
+            for line in f:
+                openfile(line)
+    show()
+
+
+
+today = datetime.today()
 d2 = today.strftime("%B %d, %Y")
+hour = datetime.now().strftime("%H")
+if hour < "12":
+    hi = "Good morning!"
+elif hour == "12":
+    hi = "Good noon!"
+elif hour < "18":
+    hi = "Good afternoon!"
+else:
+    hi = "Good evening!"
+greet = hi + ' Today is ' + d2
 
 
 root = Tk()
-
 id=StringVar()
 name = StringVar()
 year = StringVar()
-root.title('PEOPLE MANAGEMENT PROGRAM (QUANG)')
+root.title('PEOPLE MANAGEMENT PROGRAM (QUANG)' + ' - ' + 'Last Update: JAN 8,2025')
 root.configure(background='#b8c7ff')
-root.minsize(height=480,width=600)
-Label(root, text ='PEOPLE MANAGER',fg='white',font=('cambria',16),width=20,background='#1138d4').grid(row=0)
+root.minsize(height=480,width=750)
+Label(root, text ='PEOPLE MANAGER',fg='white',font=('cambria',16),width=20,background='#1138d4').grid(row=0,column=0)
+Label(root, text = greet,fg='white',font=('cambria',13),width=40,background='#1138d4').grid(row=0,column=1)
 listbox = Listbox(root,width=100,height=20,bg='#37498c',fg='white')
 listbox.grid(row=1,columnspan=2)
 show()
@@ -128,15 +165,18 @@ Label(root, text ='Name:',fg='#102887',font=('cambria',13),width=20,background='
 Entry(root,width=40,textvariable=name,background='#9daff2',selectbackground='#37498c',selectforeground='white').grid(row=3,column=1)
 Label(root, text ='Year of birth:',fg='#102887',font=('cambria',13),width=20,background='#b8c7ff').grid(row=4)
 Entry(root,width=40,textvariable=year,background='#9daff2',selectbackground='#37498c',selectforeground='white').grid(row=4,column=1)
-button = Frame(root)
-Button(button,text= 'Add',command = add,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT)
+button = Frame(root,bg='#b8c7ff')
+Button(button,text= 'Add',command = add,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT,padx=10)
 Button(button,text= 'View File', command= view,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT)
+Button(button,text= 'Import Data',command=imports,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT,padx=10)
 Button(button,text= 'Arrange', command=sort,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT)
-Button(button,text= 'Delete Last Line',command=delete,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT)
+Button(button,text= 'Delete Last Line',command=delete,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT,padx=10)
 Button(button,text= 'Delete All',command=deleteall,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT)
-Button(button,text= 'Exit',command=exit,background='#1138d4',foreground='white',activebackground='#365df7',activeforeground='white').pack(side=LEFT)
+Button(button,text= 'Exit',command=exit,background='#d41111',foreground='white',activebackground='#d63333',activeforeground='white').pack(side=LEFT,padx=10)
 button.grid(row=5,column=1)
 
-Label(root, text =d2,fg='white',font=('cambria',13),width=20,background='#1138d4').grid(row=0,column=1)
+
+
+
 root.protocol('WM_DELETE_WINDOW', msg)
 root.mainloop()
